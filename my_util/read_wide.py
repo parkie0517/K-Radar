b32dc196cac6410c8339fea6cee0068e5fc3220d
Heejun Park <parkie0517@gmail.com>
2025-01-15 23:24:10 +0900

    save

--- my_util/read_wide.py
+++ my_util/read_wide.py
@@ -1,12 +1,11 @@
 import numpy as np
 
 # Load the .npy file
-file_path = '/mnt/heejun_ssd/dataset/1/sparse_cube/cube_00033.npy'
+file_path = '/mnt/heejun_ssd/sparse_radar_tensor_wide_range/rtnh_wider_1p_1/1/sprdr_00033.npy'
 data = np.load(file_path)
 
 # Print the shape and type of the data
 print(f"Shape of the data: {data.shape}")
 print(f"Data type: {data.dtype}")
-breakpoint()
 # Optionally, print the data itself
 print(data[:10])
