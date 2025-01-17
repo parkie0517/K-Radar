d19d5abcdb67252ad71546ecf24a19d42a67af12
Heejun Park <parkie0517@gmail.com>
2025-01-17 11:40:34 +0900

    create 5D -> 4D radar convert file

---
+++ my_util/convert_5D_to_4D.py
@@ -0,0 +1,26 @@
+"""
+This file is used to convert 5D radar point cloud data to 4D radar point cloud data.
+[x, y, z, intensity, doppler] -> [x, y, z, dpopler]
+"""
+
+import numpy as np
+
+def convert_5D_to_4D(input_file, output_file):
+    # Load the 5D radar point cloud data
+    radar_data_5D = np.load(input_file) # shape is (150000, 5)
+    
+    # Check if the input data has 5 columns
+    if radar_data_5D.shape[1] != 5:
+        raise ValueError("Input data must have 5 columns: [x, y, z, intensity, doppler]")
+    
+    # Extract x, y, z, and doppler (skip intensity)
+    radar_data_4D = radar_data_5D[:, [0, 1, 2, 4]] # shape is (150000, 4)
+    
+    # Save the 4D radar point cloud data
+    np.save(output_file, radar_data_4D)
+    print(f"Converted 5D data to 4D and saved to {output_file}")
+
+# Example usage
+input_file = '/mnt/heejun_ssd/rtnh_wider_1p_1_doppler/13/rdr_sparse_doppler_00069.npy'
+output_file = './KRadar/my_util/visualize_filtered_rpc-doppler/rpc_before.npy'
+convert_5D_to_4D(input_file, output_file)
--- my_util/radar_filtering.py
+++ my_util/radar_filtering.py
@@ -1,15 +1,43 @@
+import numpy as np
+
+
+
+
+
+####################################################################################################
+####################################################################################################
+####################################################################################################
+
+
+
+
+
 def filter_roi(self, dict_item):
 
-    x_min, y_min, z_min, x_max, y_max, z_max = self.roi.xyz
-    list_keys = self.roi.keys
-    for temp_key in list_keys:
-        if temp_key in ['rdr_sparse', 'ldr64']:
-            temp_data = dict_item[temp_key] # temp_data가 포인트 클라우드 데이터임
-            temp_data = temp_data[np.where(
-                (temp_data[:, 0] > x_min) & (temp_data[:, 0] < x_max) &
-                (temp_data[:, 1] > y_min) & (temp_data[:, 1] < y_max) &
-                (temp_data[:, 2] > z_min) & (temp_data[:, 2] < z_max))]
-            dict_item[temp_key] = temp_data
-        # elif temp_key == 'label': # moved to dict item
+    x_min, y_min, z_min, x_max, y_max, z_max = roi
+    temp_data = 
+
+    temp_data = dict_item[temp_key] # temp_data가 포인트 클라우드 데이터임
+    temp_data = temp_data[np.where(
+        (temp_data[:, 0] > x_min) & (temp_data[:, 0] < x_max) &
+        (temp_data[:, 1] > y_min) & (temp_data[:, 1] < y_max) &
+        (temp_data[:, 2] > z_min) & (temp_data[:, 2] < z_max))]
+    dict_item[temp_key] = temp_data
     
     return dict_item
+
+
+
+
+
+####################################################################################################
+####################################################################################################
+####################################################################################################
+
+
+
+# x_min, y_min, z_min, x_max, y_max, z_max
+roi = 0.,-16.,-2.,72.,16.,7.6 # this is from cfg_RTNH_wide.yml
+
+rpc_before = np.load('./KRadar/my_util/visualize_filtered_rpc-doppler/rpc_before.npy')
+
