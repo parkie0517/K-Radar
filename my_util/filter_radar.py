b8dd5587a958cba1b445c90248759268c4b6ac1c
Heejun Park <parkie0517@gmail.com>
2025-01-17 11:51:27 +0900

    Fix load problem

--- my_util/filter_radar.py
+++ my_util/filter_radar.py
@@ -1,3 +1,7 @@
+"""
+This file is used to filter the radar data based on the region of interest (ROI) defined in the configuration file.
+"""
+
 import numpy as np
 
 
@@ -12,19 +16,20 @@ import numpy as np
 
 
 
-def filter_roi(self, dict_item):
+def filter_roi(rpc_before, roi, save_path):
 
     x_min, y_min, z_min, x_max, y_max, z_max = roi
-    temp_data = 
+    
+    temp_data = np.load(rpc_before) # shape is (150000, 4)
 
-    temp_data = dict_item[temp_key] # temp_data가 포인트 클라우드 데이터임
     temp_data = temp_data[np.where(
         (temp_data[:, 0] > x_min) & (temp_data[:, 0] < x_max) &
         (temp_data[:, 1] > y_min) & (temp_data[:, 1] < y_max) &
         (temp_data[:, 2] > z_min) & (temp_data[:, 2] < z_max))]
-    dict_item[temp_key] = temp_data
+
     
-    return dict_item
+    np.save(save_path, temp_data) # shape is (64612, 4)
+    print(f"Filtered data saved to {save_path}")
 
 
 
@@ -39,5 +44,6 @@ def filter_roi(self, dict_item):
 # x_min, y_min, z_min, x_max, y_max, z_max
 roi = 0.,-16.,-2.,72.,16.,7.6 # this is from cfg_RTNH_wide.yml
 
-rpc_before = np.load('./KRadar/my_util/visualize_filtered_rpc-doppler/rpc_before.npy')
-
+rpc_before = './my_util/visualize_filtered_rpc_doppler/rpc_before.npy'
+save_path = './my_util/visualize_filtered_rpc_doppler/rpc_after.npy'
+filter_roi(rpc_before, roi, save_path)
